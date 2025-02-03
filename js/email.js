a81c16fbfc180ff62b010876e342ba8424610c0f
joelsonRamos <joelson.engcomp@gmail.com>
2025-02-03 17:42:02 +0000

    configuração email segurança

--- js/email.js
+++ js/email.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Enviar os dados para a API /api/sendEmail.js
         try {
-            const response = await fetch(`${window.location.origin}/api/sendEmail`, {
+            const response = await fetch('/api/sendEmail', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
